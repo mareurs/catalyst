639634195e74b43b05db386cc9af6483509aa39f
mamar <ailinca.marius@gmail.com>
2018-01-26 21:57:24 +0200

    Add auto installer/activator

---
+++ activate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+VENV_FOLDER=catalyst-venv
+if [ ! -d $VENV_FOLDER ]; then
+    virtualenv catalyst-venv
+    source $VENV_FOLDER/bin/activate
+    pip install enigma-catalyst matplotlib    
+else
+    source $VENV_FOLDER/bin/activate
+fi
