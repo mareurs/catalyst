08d406b88ddb217eb0d460ee47a9b751aea274bb
mamar <ailinca.marius@gmail.com>
2018-01-27 13:11:55 +0200

    Switch venv to python2

--- activate.sh
+++ activate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 VENV_FOLDER=catalyst-venv
 if [ ! -d $VENV_FOLDER ]; then
-    virtualenv catalyst-venv
+    virtualenv catalyst-venv -p /usr/bin/python2
     source $VENV_FOLDER/bin/activate
     pip install enigma-catalyst matplotlib TA-lib
 else
